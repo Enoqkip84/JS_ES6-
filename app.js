77db58fbc156951d2dee0f0942b6165baea24dfc
Enoqkip84 <enoq.kipchirchir@gmail.com>
2025-12-23 09:19:29 +0800

    files

--- app.js
+++ app.js
@@ -2,5 +2,40 @@
 =============================================
 ES6+ Module
 =============================================
+- Vanilla and frameworks e.g REACT, e.t.c
 */
-ES6;
+
+/*
+=============================================
+var, let and const 
+=============================================
+var - we can define, update, and redefine variables using var
+let - can define, and update but cannot redefine
+const - cannot redefine,  mutate primitive type
+*/
+// var
+// define
+// var number = 100;
+// console.log(number);
+// // update
+// number = 200;
+// console.log(number);
+// //redefine
+// var number = 'orange';
+// console.log(number);
+
+// let
+// let amount = 100;
+// console.log(amount);
+// amount = 200;
+// console.log(amount);
+// let amount = 'orange';
+// console.log(amount);
+
+// const- cannot be reassign using primitive data type eg string and number, however, we can reassign by reference by type e,g object, array, functions. The value can mutate
+// const total = 100;
+// console.log(total);
+// // total = 200;
+// const person = { name: 'bob' };
+// person.name = 'john';
+// console.log(person.name);
