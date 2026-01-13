cc1d28097b970ca71ec13da6703c5307b0957cc5
Enoqkip84 <enoq.kipchirchir@gmail.com>
2026-01-13 10:31:56 +0800

    files

--- app.js
+++ app.js
@@ -577,3 +577,12 @@ const numbers = [23, 45, 66, 88, 2345];
 
 // console.log(Math.max(4, 5, 6, 7, 88, 99));
 console.log(Math.max(...numbers));
+
+const john = ['John', 'sanders'];
+
+const sayHello = (name, lastName) => {
+  console.log(`Hello ${name} ${lastName}`);
+};
+
+sayHello(john[0], john[1]);
+sayHello(...john);
