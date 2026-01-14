ff39f8449e6d4586ce4f4b921b71fa248ac18ce0
Enoqkip84 <enoq.kipchirchir@gmail.com>
2026-01-14 13:21:59 +0800

    files

--- app.js
+++ app.js
@@ -715,3 +715,31 @@ find, findIndex, every, some
 -every- every item in the array
 -some - at least one item
 */
+
+const people = [
+  { id: 1, name: 'john' },
+  { id: 2, name: 'peter' },
+  { id: 3, name: 'anna' },
+];
+
+// const anna = people.filter((person) => person.name == 'anna');
+
+// console.log(anna);
+// console.log(anna[0].name);
+// console.log(anna[0].id);
+
+// find
+// const anna = people.find((person) => person.name === 'anna');
+// console.log(anna);
+
+const person = people.findIndex((item) => item.id === 3);
+console.log(person);
+const newPeople = people.slice(0, person);
+console.log(newPeople);
+
+const grades = ['A', 'B', 'A', 'B', 'C'];
+const goodGrades = ['A', 'B', 'A', 'B'];
+const allGoodGrades = goodGrades.every((grade) => grade !== 'C');
+console.log(allGoodGrades);
+const oneBadGrade = grades.some((grades) => grades === 'C');
+console.log(oneBadGrade);
